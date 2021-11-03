339737dfa6ac479d9aeb7a141aaedfb008561173
SirMartin <sirmartin@gmail.com>
2021-11-03 13:57:38 +0100

    First test case passed.

--- src/NextailKata/Checkout.cs
+++ src/NextailKata/Checkout.cs
@@ -7,18 +7,21 @@ namespace NextailKata
         public Checkout(List<Discount> discounts)
         {
             Discounts = discounts;
+            BoughtItems = new List<Item>();
         }
 
         public List<Discount> Discounts { get; }
 
+        public List<Item> BoughtItems { get; }
+
         public void Scan(Item item)
         {
-            
+            BoughtItems.Add(item);
         }
 
         public decimal CalculateTotal()
         {
-            return 0;
+            return BoughtItems.Sum(x => x.Price);
         }
     }
 }
